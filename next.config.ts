52b6e896e599e5835f378cd56566590ddd1e5311
Pola <your-email@example.com>
2026-04-04 08:02:55 +0100

    e

--- next.config.ts
+++ next.config.ts
@@ -75,6 +75,19 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, s-maxage=3600',
+          },
+        ],
+      },
     ];
   },
   async redirects() {
